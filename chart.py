3442bd65c5eb6b0caaff898ecc5c30203fe6ed6a
YongXin Shi <shiyongxin@aliyun.com>
2017-04-29 22:35:30 +0800

    rename chart.py add

---
+++ chart.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author caoxm-me
+# Email
+# Date
+
+# Contributor YongXin SHI <shiyongxing@aliyun.com>
+# Last_update_time_at 2017/4/29 
+
+
+#import numpy as np
+
+# pylab 在 python-matplotlib 模块中
+import pylab as pl
+
+import xlrd as xl
+
+def open_excel(file= 'file.xls'):
+    try:
+        data = xl.open_workbook(file)
+        return data
+    except Exception.e:
+        print(str(e))
+
+# 打开表格，file为excel文件对象
+file=open_excel('c4h10.xlsx')
+
+# 按序号获取excel工作表，sheets（）返回一个列表，由下表指定工作表
+table=file.sheets()[0]
+
+# 按列获取工作表数据，第一列是波长
+wave=table.col_values(0)
+
+# 按列获取工作表数据，第二列是强度，以后同理，每两列为一组（波长，强度）
+intensity=table.col_values(1)
+print(intensity)
+# 绘图
+pl.plot(wave, intensity)
+# 显示图像
+pl.show()
