9b716f5688782331c5c5998c9c8d2fbc668f9505
YongXin Shi <shiyongxin@aliyun.com>
2017-04-29 23:00:25 +0800

    update chart.py

--- chart.py
+++ chart.py
@@ -5,10 +5,8 @@
 # Date
 
 # Contributor YongXin SHI <shiyongxing@aliyun.com>
-# Last_update_time_at 2017/4/29 
+# Last_update_time_at 2017/4/29
 
-
-#import numpy as np
 
 # pylab 在 python-matplotlib 模块中
 import pylab as pl
@@ -22,19 +20,28 @@ def open_excel(file= 'file.xls'):
     except Exception.e:
         print(str(e))
 
-# 打开表格，file为excel文件对象
-file=open_excel('c4h10.xlsx')
+def draw_chart(num):
 
-# 按序号获取excel工作表，sheets（）返回一个列表，由下表指定工作表
-table=file.sheets()[0]
+    # 打开表格，file为excel文件对象
+    file=open_excel('c4h10.xlsx')
 
-# 按列获取工作表数据，第一列是波长
-wave=table.col_values(0)
+    # 按序号获取excel工作表，sheets（）返回一个列表，由下表指定工作表
+    table=file.sheets()[0]
 
-# 按列获取工作表数据，第二列是强度，以后同理，每两列为一组（波长，强度）
-intensity=table.col_values(1)
-print(intensity)
-# 绘图
-pl.plot(wave, intensity)
+    # 按列获取工作表数据，第一列是波长
+    wave=table.col_values(0)
+
+    # 按列获取工作表数据，第二列是强度，以后同理，每两列为一组（波长，强度）
+    intensity=table.col_values(num)
+    #print(intensity)
+    # 绘图
+    pl.plot(wave, intensity)
+
+
+for num_n in [1, 3, 5, 7, 9]:
+    #print(num_n)
+    draw_chart(num_n)
+
 # 显示图像
 pl.show()
+pl.close()
