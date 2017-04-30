4a23bf437364ebc4afa45cda4ebbca503a365c76
YongXin Shi <shiyongxin@aliyun.com>
2017-04-30 18:52:32 +0800

    predata

--- chart.py
+++ chart.py
@@ -23,6 +23,28 @@ def open_excel(file= 'file.xls'):
     except Exception.e:
         print(str(e))
 
+def predata(data):
+    temp = []
+    for i in (range(len(data)-2)):
+        if not data[i+2] < data[i+1] > data[i]:
+
+            temp.append(i+1)
+            #print(temp)
+            #data[i+1] = 0
+    for i in temp:
+        data[i] = 0
+    return data
+
+def runa(data):
+    i = 0
+    print(i)
+    while i < 100:
+        data = predata(data)
+        i = i+1
+        print(i)
+    return data
+
+
 def draw_chart(num):
 
     # 打开表格，file为excel文件对象
@@ -38,7 +60,7 @@ def draw_chart(num):
     intensity = table.col_values(num)
     #print(intensity)
     # 绘图
-    pl.plot(wave, intensity)
+    pl.plot(wave, runa(intensity))
 
 
 for num_n in [1, 3, 5, 7, 9]:
